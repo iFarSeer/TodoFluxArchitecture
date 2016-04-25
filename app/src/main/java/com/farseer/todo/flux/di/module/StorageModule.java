cf3effa3d4fbddae5b609a27955e94b8cad94792
farseer <farseer.spade@gmail.com>
2016-04-26 00:07:51 +0800

    [add]:添加LICENSE

--- app/src/main/java/com/farseer/todo/flux/di/module/StorageModule.java
+++ app/src/main/java/com/farseer/todo/flux/di/module/StorageModule.java
@@ -1,3 +1,20 @@
+/*
+ *
+ *    Copyright 2016 zhaosc
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+
 package com.farseer.todo.flux.di.module;
 
 import com.farseer.todo.flux.BuildConfig;
@@ -13,9 +30,12 @@ import dagger.Module;
 import dagger.Provides;
 
 /**
- * Created by zhaosc on 16/4/24.
+ * Storage Module
+ *
+ * @author zhaosc
+ * @version 1.0.0
+ * @since 2016-04-18
  */
-
 @Module
 public class StorageModule {
 
